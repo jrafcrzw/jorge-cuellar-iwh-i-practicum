ad832d681448a025055e3808a7a5efc8965520e4
jrafcrzw <dev@jrafcrz.work>
2026-04-08 18:58:23 -0600

    feat(route): add GET route for update form and updates.pug template

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
+// * Code for Route 1 goes here
+
 // ROUTE 1 - Homepage: GET list of Pets custom object
 app.get('/', async (req, res) => {
     const url = 'https://api.hubapi.com/crm/objects/2026-03/2-60512224';
@@ -35,11 +37,20 @@ app.get('/', async (req, res) => {
     }
 });
 
-// * Code for Route 1 goes here
-
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+
+// ROUTE 2 - Form page: GET the form to create a new Pet record
+app.get('/update-cobj', async (req, res) => {
+    try {
+        res.render('updates', { 
+            title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' 
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
